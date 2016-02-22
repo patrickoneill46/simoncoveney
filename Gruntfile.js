911fdf0a317d5b7b0ff776fd5f278f40ae935b1b
Patrick O'Neill <patrick.oneill46@gmail.com>
2016-02-22 22:44:13 +0000

    Fixing indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,77 +1,83 @@
 module.exports = function(grunt) {
 
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-embed-fonts');
-    grunt.loadNpmTasks('grunt-includes');
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-embed-fonts');
+  grunt.loadNpmTasks('grunt-http-server');
+  grunt.loadNpmTasks('grunt-includes');
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-        includes: {
-          files: {
-            src: ['index.html', 'manifesto.html', 'output.css'], // Source files
-            dest: 'dist', // Destination directory
-            flatten: true,
-            cwd: '.',
-            options: {
-              silent: true,
-              banner: '<!-- Developed by Patrick O\'Neill -->'
-            }
-          },
-        },
-
-        cssmin: {
-          options: {
-            shorthandCompacting: false,
-            roundingPrecision: -1
-          },
-          target: {
-            files: {
-              'output.css': ['tmp/styles.css']
-            }
-          }
-        },
-
-        clean: ['dist/', 'tmp/'],
-
-        copy: {
-          img: {
-            src: ['img/*.png'],
-            dest: 'dist/',
-          },
-          fonts: {
-            src: ['fonts/gotham-bold-webfont.*', 'fonts/GothamBook.*'],
-            dest: 'dist/'
-          }
-        },
-
-        embedFonts: {
-          all: {
-            files: {
-              'tmp/fonts.css': ['fonts.css']
-            }
-          }
-        },
-
-        less: {
-            development: {
-              files: {
-                  'tmp/styles.css': 'less/main.less'
-              },
-            }
-        },
-
-        watch: {
-            less: {
-                files: ['less/*less'],
-                tasks: ['less']
-            }
+    includes: {
+      files: {
+        src: ['index.html', 'manifesto.html', 'output.css'], // Source files
+        dest: 'dist', // Destination directory
+        flatten: true,
+        cwd: '.',
+        options: {
+          silent: true,
+          banner: '<!-- Developed by Patrick O\'Neill -->'
         }
-    });
+      },
+    },
 
-    grunt.registerTask('default', ['clean', 'less', 'cssmin', 'includes', 'copy']);
-}
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          'output.css': ['tmp/styles.css']
+        }
+      }
+    },
+
+    clean: ['dist/', 'tmp/'],
+
+    copy: {
+      img: {
+        src: ['img/*.png'],
+        dest: 'dist/',
+      },
+      fonts: {
+        src: ['fonts/gotham-bold-webfont.*', 'fonts/GothamBook.*'],
+        dest: 'dist/'
+      },
+      styles: {
+        src: ['tmp/output.css'],
+        dest: 'dist/'
+      }
+    },
+
+    embedFonts: {
+      all: {
+        files: {
+          'tmp/fonts.css': ['fonts.css']
+        }
+      }
+    },
+
+    less: {
+      development: {
+        files: {
+            'tmp/styles.css': 'less/main.less'
+        },
+      }
+    },
+
+    watch: {
+      less: {
+        files: ['less/*.less'],
+        tasks: ['less', 'copy:styles']
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['clean', 'less', 'cssmin', 'includes', 'copy', 'watch']);
+};
